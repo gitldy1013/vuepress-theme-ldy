51da037c266e4cd04e62ec0d2708343371516468
liudongyang <13691363167@163.com>
2019-11-14 14:04:24 +0800

    init

---
+++ layouts/Tags.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="tags-wrapper" :class="recoShow?'reco-show': 'reco-hide'">
+    <Common :sidebar="false" :isComment="false">
+      <TagList :currentTag="currentTag" @getCurrentTag="tagClick"></TagList>
+      <note-abstract
+        class="list"
+        :data="posts"
+        :currentPage="currentPage"
+        :currentTag="currentTag"
+        @currentTag="getCurrentTag"></note-abstract>
+
+      <pagation
+        class="pagation"
+        :total="posts.length"
+        :currentPage="currentPage"
+        @getCurrentPage="getCurrentPage"></pagation>
+    </Common>
+  </div>
+</template>
+
+<script>
+import Common from '@theme/components/Common.vue'
+import TagList from '@theme/components/TagList.vue'
+import NoteAbstract from '@theme/components/NoteAbstract.vue'
+import mixin from '@theme/mixins/index.js'
+
+export default {
+  mixins: [mixin],
+  components: { Common, NoteAbstract, TagList },
+  data () {
+    return {
+      tags: [],
+      currentTag: '全部',
+      currentPage: 1,
+      recoShow: false,
+      allTagName: '全部'
+    }
+  },
+  computed: {
+    // 时间降序后的博客列表
+    posts () {
+      const currentTag = this.currentTag
+      let posts = []
+
+      if (currentTag !== '全部') {
+        posts = this.$tags.map[currentTag].pages
+      } else {
+        posts = this.$site.pages
+      }
+
+      posts = this._filterPostData(posts)
+      this._sortPostData(posts)
+      posts = posts.length == 0 ? [] : posts
+      this._setPage(1)
+      return posts
+    }
+  },
+  created () {
+    if (this.$tags.list.length > 0) {
+      this.currentTag = this.$route.query.tag ? this.$route.query.tag : this.currentTag
+    }
+  },
+
+  mounted () {
+    this.recoShow = true
+  },
+
+  methods: {
+
+    tagClick (currentTag) {
+      this.currentTag = currentTag
+      window.scrollTo(0, 0)
+    },
+
+    getCurrentTag (tag) {
+      this.$emit('currentTag', tag)
+    },
+
+    getCurrentPage (page) {
+      this._setPage(page)
+      setTimeout(() => {
+        window.scrollTo(0, 0)
+      }, 100)
+    },
+    _setPage (page) {
+      this.currentPage = page
+      this.$page.currentPage = page
+    }
+  }
+}
+</script>
+
+<style src="../styles/theme.styl" lang="stylus"></style>
+
+<style lang="stylus" scoped>
+@require '../styles/loadMixin.styl'
+.tags-wrapper
+  max-width: 740px;
+  margin: 0 auto;
+  padding: 4.6rem 2.5rem 0;
+  &.reco-hide {
+    .tags, .list, .pagation {
+      load-start()
+    }
+  }
+  &.reco-show {
+    .tags {
+      load-end(0.08s)
+    }
+    .list {
+      load-end(0.16s)
+    }
+    .pagation {
+      load-end(0.24s)
+    }
+  }
+
+@media (max-width: $MQMobile)
+  .tags-wrapper
+    padding: 5rem 0.6rem 0;
+</style>
---
+++ mixins/index.js
@@ -0,0 +1,28 @@
+export default {
+  methods: {
+    _tagColor () {
+      // 红、蓝、绿、橙、灰
+      const tagColorArr = ['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87', '#e15b64', '#f47e60', '#f8b26a', '#f26d6d', '#67cc86', '#fb9b5f', '#3498db']
+      const index = Math.floor(Math.random() * tagColorArr.length)
+      return tagColorArr[index]
+    },
+    _filterPostData (posts, isTimeline) {
+      posts = posts.filter(item => {
+        const { title, frontmatter: { home, date, publish }} = item
+        return isTimeline === true
+          ? !(home == true || title == undefined || date === undefined || publish === false)
+          : !(home == true || title == undefined || publish === false)
+      })
+      return posts
+    },
+    _sortPostData (posts) {
+      posts.sort((a, b) => {
+        return this._getTimeNum(b) - this._getTimeNum(a)
+      })
+    },
+    // 获取时间的数字类型
+    _getTimeNum (date) {
+      return parseInt(new Date(date.frontmatter.date).getTime())
+    }
+  }
+}
